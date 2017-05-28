b481358878e9a1fb79311dee42fda7eb4fdc24e4
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2017-05-28 17:38:31 +0300

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GithubService } from './services/github.service';
 
 @Component({
@@ -7,5 +7,45 @@ import { GithubService } from './services/github.service';
   styleUrls: ['./app.component.css'],
   providers: [GithubService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user: any;
+  repos: any[];
+  userName: string;
+
+  constructor(private _githubService: GithubService) { }
+
+  ngOnInit() {
+    this.updateData();
+  }
+
+  searchUser() {
+    if (!this.userName.trim().length) {
+      this.resetProfile();
+
+      return false;
+    }
+
+    this.updateData(this.userName);
+  }
+
+  private updateData(userName?: string) {
+    if (userName) {
+      this._githubService.updateUser(userName);
+    }
+
+    this._githubService.getUser()
+      .subscribe(user => {
+        this.user = user;
+      });
+
+    this._githubService.getRepos()
+      .subscribe(repos => {
+        this.repos = repos;
+      });
+  }
+
+  private resetProfile() {
+    this.user = null;
+    this.repos = null;
+  }
 }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,40 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubService } from '../../services/github.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-  user: any;
-  repos: any[];
-  userName: string;
-
-  constructor(private _githubService: GithubService) { }
-
-  ngOnInit() {
-    this.user = null;
-  }
-
-  searchUser() {
-    if (!this.userName.trim().length) {
-      this.user = null;
-      this.repos = null;
-
-      return false;
-    }
-
-    this._githubService.updateUser(this.userName);
-
-    this._githubService.getUser()
-      .subscribe(user => {
-        this.user = user;
-      });
-
-    this._githubService.getRepos()
-      .subscribe(repos => {
-        this.repos = repos;
-      });
-  }
+export class ProfileComponent {
+  @Input() user: any;
+  @Input() repos: any[];
 }
